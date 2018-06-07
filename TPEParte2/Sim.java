e0b36af591b0f349b8f2938fd4123b8cb7e1c474
Fernando Sandoval <fdsandoval@gmail.com>
2018-06-07 07:30:13 -0300

    Agregado comentarios sobre funcionamiento de la herramienta en clase principal

--- TPEParte2/Sim.java
+++ TPEParte2/Sim.java
@@ -4,9 +4,18 @@ public class Sim {
 
 	public static void main(String[] args) {
 		Grafo g1 = new GrafoNoDirigido();
+		//aqui va el path al archivo csv. para cargar los datos en memoria 
+		//Por favor, ingrese aquí el path a su archivo local
 		g1 = CSVReader.graphLoader("c:/users/El Ente/Desktop/DataSets/dataset3.csv");
+		// aqui se resuelve al primer servicio con el metodo BXG 
+		// (Busqueda por género) con los parámetros "Nombre del genero A" (String) y 
+		// número N (int) de géneros más buscados
 		g1.BXG("viajes",10);
+		// aqui se resuelve el segundo servicio llamando al método GBG
+		// (géneros luego de buscar por un género) con el parámetro "Género A" (String)
 		g1.GBG("viajes");
+		// por último resolvemos el tercer servicio (grafo con géneros afines)
+		// con el método DFS_Ciclo que tiene como parámetro el género inicial (String)
 		g1.DFS_Ciclo("viajes");
 	}
 }
